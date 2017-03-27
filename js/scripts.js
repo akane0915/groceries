834d52971a2b16e62e5a4d5fa3efd12b10b673fe
Asia Kane and Aubrey Masten <asia@>
2017-03-27 10:54:30 -0700

    add js script to collect inputs from user, hide form, and display grocery list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-(function(){
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    $("form").hide();
 
+    var inputs = ["item0", "item1", "item2", "item3"];
 
+    var unsorted = inputs.map(function(input){
+      return $("#" + input).val().toLowerCase();
+    });
+
+    unsorted.forEach(function(item) {
+      $("#unsorted-list").append("<li>"+item+"</li>");
+    });
+
+  });
 });
